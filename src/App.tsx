d5360f3db000ef4188584486d8cb194f23d92bba
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-03-23 20:48:36 +0100

    Update transition

--- src/App.tsx
+++ src/App.tsx
@@ -21,38 +21,30 @@ export default function App() {
 
   return (
     <div className="cv-app flex flex-col">
-      <div className="fixed bg-black w-full z-30">
-        {activeView === "grotte-passe" && (
-          <h1
-            className="mb-0"
-            style={{
-              fontSize: scrollTop > 0 ? "1.5rem" : "2rem",
-              lineHeight: scrollTop > 0 ? "1.5rem" : "2rem",
-              padding:
-                scrollTop > 0 ? "20px 40px 15px 40px" : "30px 40px 20px 40px",
-              transition: "all 0.1s ease-out",
-            }}
-          >
-            La Grotte du Passé
-          </h1>
-        )}
-        {activeView === "grotte-present" && (
-          <h1
-            className="mb-0"
-            style={{
-              fontSize: scrollTop > 0 ? "1.5rem" : "2rem",
-              lineHeight: scrollTop > 0 ? "1.5rem" : "2rem",
-              padding:
-                scrollTop > 0 ? "20px 40px 15px 40px" : "30px 40px 20px 40px",
-              transition: "all 0.1s ease-out",
-            }}
-          >
-            La Grotte du Présent
-          </h1>
-        )}
+      <div
+        className="fixed bg-black/90 w-full z-30"
+        style={{
+          height: scrollTop > 0 ? "40px" : "80px",
+        }}
+      >
+        <div
+          className="origin-top-left"
+          style={{
+            transform: scrollTop > 0 ? "scale(0.6)" : "scale(1)",
+            padding: scrollTop > 0 ? "20px" : "30px 40px 20px 40px",
+            transition: "transform 0.1s linear",
+          }}
+        >
+          {activeView === "grotte-passe" && (
+            <h1 className="mb-0 ">La Grotte du Passé</h1>
+          )}
+          {activeView === "grotte-present" && (
+            <h1 className="mb-0">La Grotte du Présent</h1>
+          )}
+        </div>
       </div>
 
-      <main className="cv-content flex-1 w-full pt-[100px] pb-[80px]">
+      <main className="cv-content flex-1 w-full pt-[80px] pb-[80px]">
         <Content />
       </main>
       <footer className="cv-footer w-full h-[80px] flex justify-center fixed z-20 bg-black bottom-0">
