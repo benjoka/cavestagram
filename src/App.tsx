0563bacc88c222339b96657df6f7e20c4d7d6381
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-03-23 11:02:44 +0100

    Add install button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import Content from "components/layout/Content";
 import Header from "components/layout/Header";
 import Navigation from "components/layout/Navigation";
-import { useAppStore } from "stores/AppStore";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  const { activeView } = useAppStore();
+  const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState<any>(null);
 
+  useEffect(() => {
+    const handler = (e: any) => {
+      e.preventDefault();
+      setSupportsPWA(true);
+      setPromptInstall(e);
+    };
+    window.addEventListener("beforeinstallprompt", handler);
+    return () => window.removeEventListener("transitionend", handler);
+  }, []);
+  const onClick = (e: any) => {
+    e.preventDefault();
+    if (promptInstall) {
+      promptInstall.prompt();
+    } else {
+      return;
+    }
+  };
   return (
     <div className="cv-app flex flex-col">
       <main className="cv-content flex-1 w-full pt-[20px] pb-[80px]">
+        <button onClick={(e: any) => onClick(e)}>Install</button>
         <Content />
       </main>
       <footer className="cv-footer w-full h-[80px] flex justify-center fixed z-20 bg-black bottom-0">
