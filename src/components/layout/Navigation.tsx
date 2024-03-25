3136b33cb9e7108f16148eb3418e03344fe669f2
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-03-25 23:02:30 +0100

    Adjust navbar

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,9 +1,7 @@
-import caveIcon from "assets/images/icons/icon_home.png";
-import caveIconFill from "assets/images/icons/icon_home_fill.png";
-import exploreIcon from "assets/images/icons/icon_lamp.png";
-import exploreIconFill from "assets/images/icons/icon_lamp_fill.png";
-import participateIcon from "assets/images/icons/icon_brush.png";
-import participateIconFill from "assets/images/icons/icon_brush_fill.png";
+import passeIcon from "assets/images/icons/icon_lamp.png";
+import passeIconFill from "assets/images/icons/icon_lamp_fill.png";
+import presentIcon from "assets/images/icons/icon_lighter.png";
+import presentIconFill from "assets/images/icons/icon_lighter_fill.png";
 import { useAppStore } from "stores/AppStore";
 
 export default function Navigation() {
@@ -12,24 +10,34 @@ export default function Navigation() {
   return (
     <div className="cv-app w-full h-full max-w-[500px] flex flex-row justify-around">
       <div
-        className="h-full flex items-center"
+        className="h-full flex items-start"
         onClick={() => setActiveView("grotte-passe")}
       >
-        <div className="w-[50px] h-[50px]">
-          <img src={caveIcon} width={50} height={50} className="absolute" />
+        <div className="w-[60px] h-[60px]">
+          <img
+            src={passeIcon}
+            width={50}
+            height={50}
+            className="absolute top-[10px]"
+          />
           {activeView === "grotte-passe" && (
-            <img src={caveIconFill} width={50} height={50} />
+            <img
+              src={passeIconFill}
+              width={50}
+              height={50}
+              className="mt-[10px]"
+            />
           )}
         </div>
       </div>
       <div
-        className="h-full flex items-center"
+        className="h-full flex items-start"
         onClick={() => setActiveView("grotte-present")}
       >
-        <div className="w-[50px] h-[50px]">
-          <img src={exploreIcon} width={50} height={50} className="absolute" />
+        <div className="w-[60px] h-[60px]">
+          <img src={presentIcon} width={60} height={60} className="absolute" />
           {activeView === "grotte-present" && (
-            <img src={exploreIconFill} width={50} height={50} />
+            <img src={presentIconFill} width={60} height={60} />
           )}
         </div>
       </div>
